66de48b8ce2ed76fa17f3dae2c0b9cca854aa1ae
MarioSk1 <MarioSk1@LAPTOP-BA7FRB8B>
2019-06-04 21:30:04 -0500

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/main/java/org/unitec/elementos1903/Cliente.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos1903;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author MarioSk1
+ */
+public class Cliente {
+    
+    @Id
+    private Integer id;
+    private String nombre, email;
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + '}';
+    }
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
--- src/main/java/org/unitec/elementos1903/Elementos1903Application.java
+++ src/main/java/org/unitec/elementos1903/Elementos1903Application.java
@@ -1,13 +1,34 @@
 package org.unitec.elementos1903;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Elementos1903Application {
+public class Elementos1903Application implements CommandLineRunner {
 
+    @Autowired
+    RepoCliente repoCliente;
 	public static void main(String[] args) {
 		SpringApplication.run(Elementos1903Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Insertar
+        repoCliente.save(new Cliente(1,"Mario","Maario@gmail.com"));
+        repoCliente.save(new Cliente(2,"Msk","Maario@gmail.com"));
+        repoCliente.save(new Cliente(3,"Jose","Maario@gmail.com"));
+        //Actualizar por ID
+         // repoCliente.save(new Cliente(1,"Marioo","Maario@gmail.com"));
+        //Buscar todos
+        for(Cliente c: repoCliente.findAll()){
+            //Buscar por Id
+            System.out.println(repoCliente.findById(1));
+        }
+        //Borrar
+        //repoCliente.deleteById(1);
+    }
 
 }
